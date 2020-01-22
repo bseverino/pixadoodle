df948db5ad622059d279061fa9fde4bd4e77951f
B Severino <biancasev@gmail.com>
2020-01-22 18:00:55 -0500

    Created canvas

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
 import Square from './Square'
 
+const Wrapper = styled.div`
+    width: 481px;
+`
+
+const SquareContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 482px;
+    border-top: 1px solid black;
+    border-left: 1px solid black;
+`
+
 const Canvas = props => {
+    const [grid, setGrid] = useState([...Array(1024).keys()])
+
     return (
-        <div>
-            <Square />
-        </div>
+        <Wrapper>
+            <SquareContainer>
+                {grid.map(number => (
+                    <Square
+                        key={number}
+                    />
+                ))}
+            </SquareContainer>
+        </Wrapper>
     )
 }
 
--- src/components/canvas/Square.js
+++ src/components/canvas/Square.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const StyledSquare = styled.div`
+    width: 15px;
+    height: 15px;
+    margin: 0;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+`
 
 const Square = props => {
+    const [color, setColor] = useState('#fff')
+
     return (
-        <div></div>
+        <div>
+            <StyledSquare
+                style={{ backgroundColor: color }}
+            />
+        </div>
     )
 }
 
